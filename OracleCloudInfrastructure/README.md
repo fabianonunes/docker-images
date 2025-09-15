294300b95cefcb18b9c7e6822f8a1c5cea21fd1d
fabianonunes <fparente@senado.leg.br>
2025-09-15 06:49:00 -0300

    mantém apenas Database

--- OracleCloudInfrastructure/README.md
+++
@@ -1,6 +0,0 @@
-# Oracle Cloud Infrastructure Container Images
-
-The following Oracle Cloud Infrastructure tools are available as container images:
-
-* [OCI Command Line Interface](oci-cli/)
-* [OCI Provider for Terraform](terraform-oci/)
--- OracleGoldenGate/23/bin/replace-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## Copyright (c) 2024, Oracle and/or its affiliates.
-set -e
-
-##
-##  Replace environment variables with their values in one or more text files
-##
-
-perl -pi -e 'foreach $key(sort keys %ENV){ s/\$\{$key\}/$ENV{$key}/g}' "$@"
